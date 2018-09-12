0aeb8e7ed5333ad031c82465a615f8c656679576
Gustaf Gunér <gustaf@guner.se>
2018-09-12 19:28:08 +0200

    New font, new structure, started working on animations with anime.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 const css = require('./style.scss');
+import anime from 'animejs'
+
+var nameTimeline = anime.timeline();
+
+nameTimeline
+  .add({
+    targets: '#landing #headline',
+		easing: 'easeOutExpo',
+		opacity: 0,
+		scale: 0
+  })
+  .add({
+    targets: '#landing #headline',
+    scale: 1,
+		easing: 'easeOutExpo',
+		delay: 100,
+		opacity: 1
+	})
+	.add({
+		targets: '#landing .text',
+		opacity: 1
+	})
+	.add({
+		targets: '#site-header #site-logo',
+		opacity: 1
+	});
